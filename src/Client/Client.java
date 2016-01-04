6fc1f9249b29dc0372e1c9ec1abc4477693c8b39
Tumaï <Tuma@192.168.1.11>
2016-01-04 18:25:05 +0100

    Code client + server mais exception je sais pas pourquoi. Client envoie sont message pas le server

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,5 +1,80 @@
 package Client;
 
-public class Client {
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
 
+public class Client{
+
+	public static final int DEFAULT_PORT = 8189;
+	int port;
+	
+	SocketChannel clientChannel;
+	
+	public Client() throws IOException
+	{
+		this(DEFAULT_PORT);
+	}
+	
+	public Client(int port) {
+		this.port = port;
+	}
+	
+	public void setup() throws IOException
+	{
+		this.clientChannel = SocketChannel.open();
+		clientChannel.configureBlocking(false);
+		clientChannel.connect(new InetSocketAddress(InetAddress.getLocalHost(),DEFAULT_PORT));
+		
+	}
+	
+	public void start() throws IOException
+	{
+		System.out.println("Setting up client ...");
+		this.setup();
+		System.out.println("Client ready");
+		
+		while(!clientChannel.finishConnect())
+		{
+			
+		}
+		
+	}
+
+	private  void write(String str) throws IOException
+	{
+		String message = str;
+		byte[] messages = message.getBytes();
+		ByteBuffer buffer = ByteBuffer.wrap(messages);
+		clientChannel.write(buffer);
+		buffer.clear();
+	}
+	
+	private void read() throws IOException
+	{
+		ByteBuffer buffer = ByteBuffer.allocate(512);
+		clientChannel.read(buffer);
+		String output = new String(buffer.array()).trim();
+		System.out.println("Message read from client: " + output);
+		if(output.equals("message"))
+		{
+			System.out.println("Server send a message !");
+		}
+	}
+	
+	public static void main (String [] args) throws IOException
+	{
+		Client client = new Client();
+		try {
+			client.start();
+			client.write("coucou");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 }
--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,5 +1,130 @@
 package Server;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.channels.spi.SelectorProvider;
+import java.util.Iterator;
+
 public class Server {
+	
+	public static final int DEFAULT_PORT = 8189;
+	private int port;
+	
+	//The channel on wich we'll accept connections
+	private ServerSocketChannel serverChannel;
+	
+	//The selector we'll be monitoring
+	private Selector selector;
+	private String messages = "message";
+
+	public Server()
+	{
+		this(DEFAULT_PORT);
+	}
+	public Server(int port)
+	{
+		this.port = port;
+	}
+	
+	
+	public void setup() throws IOException
+	{
+		this.selector = Selector.open();
+		
+		this.serverChannel = ServerSocketChannel.open();
+		this.serverChannel.configureBlocking(false);
+		
+		InetAddress ia = InetAddress.getLocalHost();
+		System.out.println(ia);
+		InetSocketAddress isa = new InetSocketAddress(ia,this.port);
+		this.serverChannel.socket().bind(isa);
+	}
+	
+	public void start() throws IOException
+	{
+		System.out.println("Setting up server...");
+		this.setup();
+		System.out.println("Server started...");
+		SelectionKey acceptKey = this.serverChannel.register(this.selector,  SelectionKey.OP_ACCEPT);
+		
+		while(acceptKey.selector().select()>0)
+		{
+			for(Iterator<SelectionKey> it = this.selector.selectedKeys().iterator();it.hasNext();)
+			{
+				SelectionKey key = it.next();
+				it.remove();
+				
+				if(!key.isValid())
+				{
+					continue;
+				}
+				
+				if(key.isAcceptable())
+				{
+					System.out.println("Key is Acceptable");
+					this.accept(key);
+					continue;
+				}
+				else if (key.isReadable())
+				{
+					System.out.println("Key is Readable");
+					this.read(key);
+					continue;
+				}
+				else if (key.isWritable())
+				{
+					System.out.println("Key is Writable");
+					this.write(key);
+					continue;
+				}
+			}
+		}
+	}
+	
+	private void write(SelectionKey key) throws IOException {
+		SocketChannel socket = (SocketChannel) key.channel();
+		byte [] message = new String(messages).getBytes();
+		ByteBuffer buffer = ByteBuffer.wrap(message);
+		int nBytes = socket.write(buffer);
+		buffer.clear();
+		
+	}
+	private void read(SelectionKey key) throws IOException {
+		SocketChannel socket = (SocketChannel) key.channel();
+		ByteBuffer buffer = ByteBuffer.allocate(512);
+		socket.read(buffer);
+		String output = new String(buffer.array()).trim();
+		System.out.println("Message read from client: " + output);
+		if(output.equals("coucou"))
+		{
+			System.out.println("Client send coucou !");
+			this.write(key);
+		}		
+	}
+	private void accept(SelectionKey key) throws IOException {
+		ServerSocketChannel ssc = (ServerSocketChannel)key.channel();
+		SocketChannel socket = (SocketChannel)ssc.accept();
+		socket.configureBlocking(false);
+		socket.register(this.selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+	}
+	
+	public static void main(String[] args)
+	{
+	    Server server = new Server();
+	    try {
+			server.start();
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+	}
 
 }
